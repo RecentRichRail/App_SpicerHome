4d021a663abc4b27fa776df16a3ba0b2a6ce77fe
RecentRichRail <Matthewspicer.18@gmail.com>
2024-11-28 13:26:53 +0000

    Remove unused imports from user_login module in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,9 @@ from models.user_data.network_passwords import NetworkPasswordModel
 from models.user_profile.user_profile import User
 from models.user_profile.permissions import PermissionsModel
 
-from models.user_login.webauthn import WebAuthnCredential
+# from models.user_login.webauthn import WebAuthnCredential
 from models.user_login.login_attempts import LoginAttemptModel
-from models.user_login.challenges import RegistrationChallenge, AuthenticationChallenge, EmailAuthSecret
+# from models.user_login.challenges import RegistrationChallenge, AuthenticationChallenge, EmailAuthSecret
 
 from models.user_data.banana_game.banana_game_button_press import BananaGameButtonPressModel
 from models.user_data.banana_game.banana_game_user_bananas import BananaGameUserBananasModel
