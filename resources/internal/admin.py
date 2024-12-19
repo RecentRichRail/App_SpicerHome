06dbcfb4d62b6573a348cf32c5acb77b72b77a73
RecentRichRail <Matthewspicer.18@gmail.com>
2024-12-19 22:56:49 +0000

    Refactor admin history and tracking functions; streamline permission checks and simplify response structure

--- resources/internal/admin.py
+++ resources/internal/admin.py
@@ -14,33 +14,34 @@ def admin_history():
     user_id = request.args.get('q')
     selected_info = request.args.get('info')
     # data = current_app.data
+    admin_permission = next((perm for perm in current_user.json_user_permissions() if perm["permission_name"] == "admin"), None)
+    print(admin_permission["permission_level"])
+    if not admin_permission or admin_permission["permission_level"] != 0:
+        abort(404)
     if selected_info == "history":
-        for permission in current_user.permissions:
-            if permission.permission_name == "admin" and permission.permission_level == 0:
 
-                admin_history_query = RequestsModel.query.filter_by(user_id=user_id).order_by(RequestsModel.id.desc()).all()
 
-                admin_history_query_structured = {user_id: {}}
-                for history_request in admin_history_query:
-                    if history_request.is_search == True:
-                        try:
-                            user_query_url = history_request.command.search_url.format(history_request.encoded_query)
-                        except AttributeError:
-                            user_query_url = history_request.command.url
-                    elif history_request.is_search == False:
-                        user_query_url = history_request.command.url
 
-                    admin_history_query_structured[user_id][history_request.id] = {
-                        "request_id": history_request.id,
-                        "original_request": history_request.original_request,
-                        "query_url": user_query_url,
-                        "date_and_time": history_request.datetime_of_request
-                    }
-                response = admin_history_query_structured
-            else:
-                response = {"message": "No permission for this resource."}
-                abort(404)
-        
+        admin_history_query = RequestsModel.query.filter_by(user_id=user_id).order_by(RequestsModel.id.desc()).all()
+
+        admin_history_query_structured = {user_id: {}}
+        for history_request in admin_history_query:
+            if history_request.is_search == True:
+                try:
+                    user_query_url = history_request.command.search_url.format(history_request.encoded_query)
+                except AttributeError:
+                    user_query_url = history_request.command.url
+            elif history_request.is_search == False:
+                user_query_url = history_request.command.url
+
+            admin_history_query_structured[user_id][history_request.id] = {
+                "request_id": history_request.id,
+                "original_request": history_request.original_request,
+                "query_url": user_query_url,
+                "date_and_time": history_request.datetime_of_request
+            }
+        response = admin_history_query_structured
+
         history_requests = response
         page_title = f"SpicerHome {user_id} History"
 
@@ -57,28 +58,24 @@ def admin_history():
         return render_template('internal/search/history.html', **context)
     
     elif selected_info == "track":
-        for permission in current_user.permissions:
-            if permission.permission_name == "admin" and permission.permission_level == 0:
 
-                user_track_query = TrackingNumbersModel.query.filter_by(user_id=user_id).order_by(TrackingNumbersModel.id.desc()).all()
 
-                user_track_query_structured = {user_id: {}}
-                for track_request in user_track_query:
-                    tracking = get_tracking_number(track_request.tracking_number)
-                    user_track_query_structured[user_id][track_request.id] = {
-                        "track_id": track_request.id,
-                        "tracking_number": track_request.tracking_number,
-                        "query_url": tracking.tracking_url,
-                        "courier_name": tracking.courier.name,
-                        "note": track_request.note,
-                        "is_active": track_request.is_active,
-                        "datetime_of_create_on_database": track_request.datetime_of_create_on_database
-                    }
-                response = user_track_query_structured
-            else:
-                response = {"message - No permission for this resource.": "No permission for this resource."}
-                abort(404)
-        
+        user_track_query = TrackingNumbersModel.query.filter_by(user_id=user_id).order_by(TrackingNumbersModel.id.desc()).all()
+
+        user_track_query_structured = {user_id: {}}
+        for track_request in user_track_query:
+            tracking = get_tracking_number(track_request.tracking_number)
+            user_track_query_structured[user_id][track_request.id] = {
+                "track_id": track_request.id,
+                "tracking_number": track_request.tracking_number,
+                "query_url": tracking.tracking_url,
+                "courier_name": tracking.courier.name,
+                "note": track_request.note,
+                "is_active": track_request.is_active,
+                "datetime_of_create_on_database": track_request.datetime_of_create_on_database
+            }
+        response = user_track_query_structured
+
         tracking_requests = response
         page_title = f"SpicerHome {user_id} Tracking"
 
@@ -96,16 +93,11 @@ def admin_history():
         return render_template('internal/search/tracking.html', **context)
     
     else:
-        for permission in current_user.permissions:
-            if permission.permission_name == "admin" and permission.permission_level == 0:
-                admin_users_query = User.query.order_by(User.id.desc()).all()
-                admin_user_query_structured = {}
-                for user in admin_users_query:
-                    admin_user_query_structured[user.id] = user.to_dict()
-                response = admin_user_query_structured
-            else:
-                response = {"message - No permission for this resource.": "No permission for this resource."}
-                abort(404)
+        admin_users_query = User.query.order_by(User.id.desc()).all()
+        admin_user_query_structured = {}
+        for user in admin_users_query:
+            admin_user_query_structured[user.id] = user.to_dict()
+        response = admin_user_query_structured
         users_query = response
         print(f" users = {users_query}")
 
